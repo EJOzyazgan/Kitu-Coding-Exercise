df9802595f39c4d342f76787776a538b6ef7a3a7
EJ Ozyazgan <ejozyazgan@gmail.com>
2019-05-02 14:13:00 -0700

    Updated Requests

--- app.js
+++ app.js
@@ -25,31 +25,34 @@ app.get('/users', async (req, res) => {
     }
 
     let allUsers = users.length;
+    let newUsers = 10;
     const url = 'https://randomuser.me/api';
 
-    while (users.length < allUsers + 10) {
+    for (let i = 0; i < allUsers + newUsers; i++) {
         try {
-            const response = await axios.get(url);
-            const data = response.data.results[0];
+            axios.get(url).then(response => {
+                const data = response.data.results[0];
+                let user = {
+                    gender: data.gender,
+                    firstname: data.name.first,
+                    city: data.location.city,
+                    email: data.email,
+                    cell: data.cell
+                };
 
-            let user = {
-                gender: data.gender,
-                firstname: data.name.first,
-                city: data.location.city,
-                email: data.email,
-                cell: data.cell
-            };
+                users.push(user);
 
-            users.push(user);
+                if (users.length === allUsers + newUsers) {
+                    if (!saveUsers(users))
+                        return res.status(400).json({message: 'Error saving user'});
+
+                    return res.status(200).send(users);
+                }
+            });
         } catch (err) {
-            console.error(err);
+            return res.status(400).json({message: 'Error getting users'});
         }
     }
-
-    if (!saveUsers(users))
-        return res.status(400).json({message: 'Error saving user'});
-
-    return res.status(200).send(users);
 });
 
 app.post('/users', async (req, res) => {
